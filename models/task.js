3517c93d7001c6e2a290d9f2c8c2c69ed710e2b6
swaatipandey <swati.pandey@contentstack.com>
2021-01-06 17:49:15 +0530

    deleted task.js file which was not needed

--- models/task.js
+++
@@ -1,9 +0,0 @@
-const uniqid = require("uniqid");
-class Task {
-  constructor(taskName, status = "pending") {
-    this.taskId = uniqid();
-    this.taskName = taskName;
-    this.status = "Pending";
-  }
-}
-module.exports = Task;
