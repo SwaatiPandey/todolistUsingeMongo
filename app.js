c0ceddc79f29e7d1e1849a6ba09ab6c3bd33f3a2
SwaatiPandey <swati.pandey@contentstack.com>
2021-02-22 19:41:10 +0530

    installed cors and used it in app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const router = require("./routes/todoRoutes");
 const { config } = require("process");
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/todoList", router);
